ffdadb1220f77c0e0db018ef892380dbcf1d0608
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-16 11:21:50 +0900

    카메라기능 작동시키기
    Expo에 있는 camera 기능을 가져다가
    코드를 써주니까
    핸드폰에 카메라가 생겼음.
    style 해줘서 사이즈 키워주고, border-radius도 해줬음.

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions } from 'react-native';
+import { Permissions } from 'expo';
+import styled from "styled-components";
+import { Camera } from 'expo-camera';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const { width, height } = Dimensions.get("window");
+
+const CenterView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
+
+export default class App extends React.Component {
+  state = {
+    hasPermission: null
+  };
+  componentDidMount = async() => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "granted") {
+      this.setState({ hasPermission: true });
+    } else {
+      this.setState({ hasPermission: false });
+    }
+    
+  };
+  render() {
+    const { hasPermission } = this.state;
+    if (hasPermission === true) {
+      return (
+        <CenterView>
+          <Camera 
+            style={{ 
+              width: width -40,
+              height: height / 1.5,
+              borderRadius: 10,
+              overflow: "hidden"
+              }}
+              type={Camera.Constants.Type.front} 
+            />
+        </CenterView>
+      );
+    } else if (hasPermission === false ){
+      return (
+        <CenterView>
+          <Text>Don't have permmission for this</Text>
+        </CenterView>
+      );
+    } else {
+      return (
+        <CenterView>
+          <ActivityIndicator />
+        </CenterView>
+      );
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
